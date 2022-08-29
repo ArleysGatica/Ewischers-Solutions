baae2251c6964bf3974bc41c6501808e28630a45
Arleys Gatica <gatica.arleys16@gmail.com>
2022-08-28 19:40:52 -0600

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Header from "./components/Navbar"
+import Footer from "./components/Footer"
 
 function App() {
 
     return (
         <>
             <Header />
+            <h1>j</h1>
+            <Footer />
         </>
     )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+    return (
+        <>
+            <footer className="bg-gray-800 absolute inset-x-0 bottom-0 h-16 flex items-center justify-center">
+                <h1 className="text-center text-white block px-3 py-2 rounded-md text-base font-medium">Software Development Agency</h1>
+                <p className="text-center text-gray-500">
+                    <a href="https://tailwindui.com/">Tailwind UI</a> by <a href="https://twitter.com/tailwindcss">@tailwindcss</a>
+                </p>
+            </footer>
+        </>
+    );
+}
